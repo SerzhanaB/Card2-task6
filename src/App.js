e64fc6e3386f30631d134a02e6aa5f773173c549
Serzhana <buraevase@gmail.com>
2021-08-27 23:06:10 +0300

    Добавление аддаптивного меню

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { AppBar, Container, IconButton, Toolbar, Typography, Box } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: { 
+    flexGrow:1
+  },
+  menuButton: {
+    marginRight:theme.spacing(1)
+  },
+  title: {
+    flexGrow:1
+  }
+}))
 
 function App() {
+const classes=useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <AppBar position="fixed">
+     <Container fixed>
+        <Toolbar>
+          <IconButton edge="start"
+          color="inherit" aria-laabel="menu" className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>Название сайта </Typography>
+          <Box mr={3}>
+            <Button color="inherit" variant="outlined"> Вход</Button>
+            </Box>
+            <Button color="secondary" variant="contained"> Регистрация</Button>
+        </Toolbar>
+
+     </Container>
+     </AppBar>
   );
 }
 
